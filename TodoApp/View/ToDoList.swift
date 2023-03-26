7eeb48b11ca7d5896f183b02e01cbb30bd94f095
kazukiomori <kazuki1110170501@gmail.com>
2023-03-27 07:50:49 +0900

    @FetchRequestでlist

--- TodoApp/View/ToDoList.swift
+++ TodoApp/View/ToDoList.swift
@@ -6,20 +6,34 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ToDoList: View {
     
     // MARK: Property
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \TodoEntity.time,
+                                           ascending: true)],
+        animation: .default)
+    var todoList: FetchedResults<TodoEntity>
+    
     let category: TodoEntity.Category
     
     // MARK: Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(todoList) { todo in
+                Text(todo.task ?? "no title")
+            }
+        }
     }
 }
 // MARK: Preview
 struct ToDoList_Previews: PreviewProvider {
+    
+    static let persistenceController = PersistenceController.shared
     static var previews: some View {
         ToDoList(category: .ImpUrg_1st)
+            .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
 }

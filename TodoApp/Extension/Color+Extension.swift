fb68fba7a13acd87efc8e63137714207ae6e45fb
kazukiomori <kazuki1110170501@gmail.com>
2023-03-26 16:52:55 +0900

    ColorExtensionを追加

---
+++ TodoApp/Extension/Color+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extension.swift
+//  TodoApp
+//
+//  Created by Kazuki Omori on 2023/03/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let tBackground = Color(#colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1))
+    static let tforeground = Color(#colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1))
+    static let tRed = Color(#colorLiteral(red: 1, green: 0.2902, blue: 0.4745, alpha: 1))
+    static let tBlue = Color(#colorLiteral(red: 0.0706, green: 0.6392, blue: 0.9922, alpha: 1))
+    static let tYellow = Color(#colorLiteral(red: 1, green: 0.7569, blue: 0.2235, alpha: 1))
+    static let tGreen = Color(#colorLiteral(red: 0.2157, green: 0.7255, blue: 0.5804, alpha: 1))
+    static let tTitle = Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+}

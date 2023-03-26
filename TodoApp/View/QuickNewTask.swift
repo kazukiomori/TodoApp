bf2b63d4be2d494e5b0a6bd766b4c2dca650daf9
kazukiomori <kazuki1110170501@gmail.com>
2023-03-27 00:04:33 +0900

    QuickNewTaskを作成

---
+++ TodoApp/View/QuickNewTask.swift
@@ -0,0 +1,47 @@
+//
+//  QuickNewTask.swift
+//  TodoApp
+//
+//  Created by Kazuki Omori on 2023/03/26.
+//
+
+import SwiftUI
+
+struct QuickNewTask: View {
+    
+    // MARK: Property
+    let category: TodoEntity.Category
+    
+    @State var newTask: String = ""
+    
+    // MARK: Function
+    
+    fileprivate func addNewTask() {
+        self.newTask = ""
+    }
+    
+    fileprivate func cancelTask() {
+        self.newTask = ""
+    }
+    
+    // MARK: Body
+    var body: some View {
+        HStack {
+            TextField("新しいタスク", text: $newTask)
+            Button("追加") {
+                self.addNewTask()
+            }
+            Button("Cancel") {
+                self.cancelTask()
+            }
+            .foregroundColor(.red)
+        }
+    }
+}
+
+// MARK: Preview
+struct QuickNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        QuickNewTask(category: .ImpUrg_1st)
+    }
+}

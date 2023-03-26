381272872a517bb645417f7f89703a0321ebcb7f
kazukiomori <kazuki1110170501@gmail.com>
2023-03-26 15:45:51 +0900

    チェックボックスに文字を表示する

--- TodoApp/CheckBox.swift
+++ TodoApp/CheckBox.swift
@@ -7,21 +7,41 @@
 
 import SwiftUI
 
-struct CheckBox: View {
+struct CheckBox<Label>: View where Label: View {
+    // MARK: Property
     @Binding var checked: Bool
+    private var label: () -> Label
+    
+    // MARK: initializer
+    public init(checked: Binding<Bool>,
+                @ViewBuilder label: @escaping () -> Label) {
+        //バインディング構造体にアクセスするために"_"をつける
+        self._checked = checked
+        self.label = label
+    }
+    
+    // MARK: Body
     var body: some View {
-        Image(systemName: checked ? "checkmark.circle" : "circle")
-            .onTapGesture {
-                self.checked.toggle()
+        HStack {
+            Image(systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    self.checked.toggle()
             }
+            label()
+        }
     }
 }
 
+// MARK: Preview
 struct CheckBox_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            CheckBox(checked: .constant(false))
-            CheckBox(checked: .constant(true))
+            CheckBox(checked: .constant(false)) {
+                Text("牛乳を買う")
+            }
+            CheckBox(checked: .constant(true)) {
+                
+            }
         }
     }
 }

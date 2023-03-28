bb7605c27b849eb58c85e8f7c3e31614f22192e5
kazukiomori <kazuki1110170501@gmail.com>
2023-03-29 03:02:04 +0900

    nilを扱えるようにBindingクラスにExtensionを追加

---
+++ TodoApp/Extension/Binding+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Binding+Extension.swift
+//  TodoApp
+//
+//  Created by Kazuki Omori on 2023/03/29.
+//
+
+import SwiftUI
+
+extension Binding {
+    
+    init<T>(isNotNil source: Binding<T?>, defaultValue: T) where Value == Bool {
+        self.init(get: { source.wrappedValue != nil },
+                  set: { source.wrappedValue = $0 ? defaultValue : nil })
+    }
+    
+    init(_ source: Binding<Value?>, _ defaultValue: Value) {
+        
+        self.init(get:{
+            if source.wrappedValue == nil {
+                source.wrappedValue = defaultValue
+            }
+            return source.wrappedValue ?? defaultValue
+            
+        },set:{
+            source.wrappedValue = $0
+        })
+    }
+
+}

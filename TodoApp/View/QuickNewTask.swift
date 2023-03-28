737cb9bcbd7405b07a309692243a9d31d87cd525
kazukiomori <kazuki1110170501@gmail.com>
2023-03-29 00:43:46 +0900

    保存処理の追加

--- TodoApp/View/QuickNewTask.swift
+++ TodoApp/View/QuickNewTask.swift
@@ -13,10 +13,14 @@ struct QuickNewTask: View {
     let category: TodoEntity.Category
     
     @State var newTask: String = ""
+    @Environment(\.managedObjectContext) var viewContext
     
     // MARK: Function
     
     fileprivate func addNewTask() {
+        TodoEntity.create(in: self.viewContext,
+                          category: self.category,
+                          task: self.newTask)
         self.newTask = ""
     }
     
@@ -44,7 +48,10 @@ struct QuickNewTask: View {
 
 // MARK: Preview
 struct QuickNewTask_Previews: PreviewProvider {
+    static let container = PersistenceController.shared.container
+    static let context = container.viewContext
     static var previews: some View {
         QuickNewTask(category: .ImpUrg_1st)
+            .environment(\.managedObjectContext, container.viewContext)
     }
 }
